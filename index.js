323ebe2d9ef8f20222510ed0b3b92372cfc2cc84
ShayMariza <Shay7Mariza@gmail.com>
2024-08-18 18:39:37 -0500

    JS add

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onscroll = function() {scrollProgress()};
+
+function scrollProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  
+  document.getElementById("bar").style.width = scrolled + "%";
+}
